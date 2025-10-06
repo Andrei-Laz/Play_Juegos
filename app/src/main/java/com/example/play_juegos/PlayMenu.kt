c36b54aba674ea6cf066b0c670850aff4f3aea30
Andrei-Laz <andlaz2@alu.edu.gva.es>
2025-10-06 20:52:34 +0200

    Minor change in PlayMenu Column display

--- app/src/main/java/com/example/play_juegos/PlayMenu.kt
+++ app/src/main/java/com/example/play_juegos/PlayMenu.kt
@@ -3,8 +3,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
@@ -53,7 +57,12 @@ fun PlayMenu(modifier: Modifier = Modifier) {
 
     Box(contentAlignment = Alignment.BottomEnd) {
 
-        Column() {
+        Column(
+            modifier = modifier
+                .selectableGroup()
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
             myOptions.forEach {
                 MyCheckBox(it)
             }
